631a5a781cf8864553aa5bc87d4a3206c5065da4
jfare11ano <jarellan@ccu.cl>
2019-05-07 21:29:23 -0400

    agraga comentario

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,8 +1,12 @@
+## Considero como ARGV[0]: el precio, ARGV[1]: cantidad total de usuarios, ARGV[2]: usuarios premium,ARGV[3]: usuarios gratis, ARGV[4]: gastos    
+
 pv = ARGV[0].to_f
 us = ARGV[1].to_f
 usprem = ARGV[2].to_f
 usgrat = ARGV[3].to_f
 g = ARGV[4].to_f
+
+
 
 if us < usprem or us < usprem+usgrat
     puts "Error, los usuarios deben ser mayores a los premium, a los gratis o bien mayor o igual a la suma de ambos"
